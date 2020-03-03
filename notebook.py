77bafe81af2d7680057c6601e991a1762b604825
ischeinkman <scheinkman.ilan@gmail.com>
2020-03-03 15:27:19 -0800

    Added 9-bit code; made all circuits purely quantum.

--- notebook.py
+++ notebook.py
@@ -34,15 +34,36 @@ def apply_bitflip(rho, p, bit):
             flip_matrix.append(identity(2))
     flip_op = tensor(flip_matrix)
     return (1 - p) * rho + p * flip_op * rho * flip_op.dag()
+#%% [markdown]
+# Applies a *classical* 1 bit signflip error to density matrix $rho$
+# with probability $p$. 
+#%%
+def apply_signflip(rho, p, bit):
+    flip_matrix = []
+    num_bits = int(np.log(max(rho.shape)) /np.log(2))
+    for idx in range(0, num_bits):
+        if idx == bit:
+            flip_matrix.append(sigmaz())
+        else:
+            flip_matrix.append(identity(2))
+    flip_op = tensor(flip_matrix)
+    return (1 - p) * rho + p * flip_op * rho * flip_op.dag()
 # %% [markdown]
 # Operator to apply a *quantum* 1 bit bitflip error to a single qbit. 
 # %%
-def noise(epsilon):
+def qbitflip(epsilon):
     return Qobj(np.array([
         [np.sqrt(1 - epsilon), np.sqrt(epsilon)], 
-        [np.sqrt(epsilon), np.sqrt(1 - epsilon)], 
+        [-np.sqrt(epsilon), np.sqrt(1 - epsilon)], 
     ]))
-
+# %% [markdown]
+# Operator to apply a *quantum* 1 bit phaseflip error to a single qbit. 
+# %%
+def qphaseflip(epsilon):
+    bit_mat = qbitflip(epsilon)
+    ret = snot() * bit_mat * snot() 
+    display(ret)
+    return ret
 #%% [markdown]
 # Applies an operator equivalent to $\sum_{i\in states} |i>$ to density 
 # matrix $rho$, while still preserving the dimension of $rho$. 
@@ -57,87 +78,75 @@ def project_states(rho, states):
     return ret 
 
 # %%
-max_error = 0.1
-error = max_error
-qb_errors = [error]
+def qerror(ptheta, pphi):
+    theta = ptheta * 2 * np.pi 
+    phi = pphi * np.pi 
+    return np.cos(phi) * identity(2) + np.cos(theta) * np.sin(phi) * sigmax() + np.sin(theta) * np.sin(phi) * sigmaz()
 
-#%% [markdown]
-# ## Circuit components
+#%%
+three_bit_ts = QubitCircuit(3, reverse_states=False)
+three_bit_ts.add_state('\psi', targets=[0], state_type='input')
+three_bit_ts.add_state('0', targets=[1, 2], state_type='input')
+three_bit_ts.add_gate('CNOT', controls=[0], targets = [1])
+three_bit_ts.add_gate('CNOT', controls=[0], targets = [2])
+three_bit_ts.user_gates = {'E_f' :  qbitflip, 'Flip' : sigmax, 'E_p' : qphaseflip}
+three_bit_ts.add_gate('E_f', targets=[0], arg_value=0.1, arg_label="\epsilon")
+three_bit_ts.add_gate('E_f', targets=[1], arg_value=0.1, arg_label="\epsilon")
+three_bit_ts.add_gate('E_f', targets=[2], arg_value=0.1, arg_label="\epsilon")
+three_bit_ts.add_gate('CNOT', controls=[0], targets = [1])
+three_bit_ts.add_gate('CNOT', controls=[0], targets = [2])
+three_bit_ts.add_gate('TOFFOLI', controls=[1, 2], targets=[0])
+display(three_bit_ts.png)
+display(gate_sequence_product(three_bit_ts.propagators()).tidyup())
 
-#%% [markdown]
-# ### Setup 
-# This setup circuit duplicates the qubit $|\psi>$ across 3 different qubits via
-# entanglement, creating state $<0|\psi>|0 0 0 > + <1|\psi> |1 1 1 >$. 
-# The bottom 2 qubits are the ancilla we will use to measure the syndrome. 
 # %%
-setup_circuit = QubitCircuit(5, reverse_states=False)
-setup_circuit.add_gate('CNOT', controls=[0], targets=[1])
-setup_circuit.add_gate('CNOT', controls=[0], targets=[2])
+test_inp = build_initial(1, 0, 2)
+display(test_inp.ptrace([0]))
+display((qbitflip(0.1) * basis(2, 0)).ptrace([0]))
+display((gate_sequence_product(three_bit_ts.propagators()) * test_inp).ptrace([0]).unit().tidyup())
+display((gate_sequence_product(three_bit_ts.propagators()) * test_inp).ptrace([0]).unit().tidyup().purity())
 
-#%% [markdown]
-# ### Syndrome measurement 
-# This circuit measures the "syndrome" value of the circuit; the top qbit is set to 1
-# when bit 0 $\ne$ bit 1, and the bottom is set to 1 when bit 0 $\ne$ bit 2. 
-#%%
-measure_circuit = QubitCircuit(5, reverse_states=False)
-measure_circuit.add_state('S', targets=[3, 4])
-measure_circuit.add_gate('CNOT', controls=[0], targets=[3])
-measure_circuit.add_gate('CNOT', controls=[1], targets=[3])
-measure_circuit.add_gate('CNOT', controls=[0], targets=[4])
-measure_circuit.add_gate('CNOT', controls=[2], targets=[4])
 
-#%% [markdown]
-# ### Full 3-bit Error Correction Circuit
 # %%
-three_bit = QubitCircuit(5, reverse_states=False)
-three_bit.add_state('S', targets=[3, 4], state_type='output')
-three_bit.add_state('\psi', targets=[0], state_type='input')
-three_bit.add_state('0', targets=[1, 2, 3, 4], state_type='input')
-three_bit.add_circuit(setup_circuit)
-three_bit.user_gates = {"Error" : noise}
-three_bit.add_gate('Error', targets=[0], arg_value= qb_errors[0], arg_label="\epsilon")
-three_bit.add_gate('Error', targets=[1], arg_value= qb_errors[0], arg_label="\epsilon")
-three_bit.add_gate('Error', targets=[2], arg_value= qb_errors[0], arg_label="\epsilon")
-three_bit.add_circuit(measure_circuit)
-display(three_bit.png)
+nine_bit_ts = QubitCircuit(9, reverse_states=False)
+nine_bit_ts.user_gates = {'E_f' :  qbitflip, 'Flip' : sigmax, 'E_p' : qphaseflip, 'Error' : lambda : qerror(0.4, 0.4)}
+nine_bit_ts.add_state('\psi', targets=[0], state_type='input')
+nine_bit_ts.add_state('R_{bit}', targets=[1, 2, 3, 4, 5, 6, 7, 8], state_type='input')
+nine_bit_ts.add_gate('CNOT', controls=[0], targets = [3])
+nine_bit_ts.add_gate('CNOT', controls=[0], targets = [6])
+nine_bit_ts.add_gate('SNOT', targets=[0])
+nine_bit_ts.add_gate('SNOT', targets=[3])
+nine_bit_ts.add_gate('SNOT', targets=[6])
+nine_bit_ts.add_gate('CNOT', controls=[0], targets = [1])
+nine_bit_ts.add_gate('CNOT', controls=[0], targets = [2])
+nine_bit_ts.add_gate('CNOT', controls=[3], targets = [4])
+nine_bit_ts.add_gate('CNOT', controls=[3], targets = [5])
+nine_bit_ts.add_gate('CNOT', controls=[6], targets = [7])
+nine_bit_ts.add_gate('CNOT', controls=[6], targets = [8])
+for idx in range(0, 9):
+    nine_bit_ts.add_gate('Error', targets=[idx],  arg_label="\epsilon")
+nine_bit_ts.add_gate('CNOT', controls=[0], targets = [1])
+nine_bit_ts.add_gate('CNOT', controls=[0], targets = [2])
+nine_bit_ts.add_gate('TOFFOLI', controls=[1, 2], targets=[0])
+nine_bit_ts.add_gate('CNOT', controls=[3], targets = [4])
+nine_bit_ts.add_gate('CNOT', controls=[3], targets = [5])
+nine_bit_ts.add_gate('TOFFOLI', controls=[4, 5], targets=[3])
+nine_bit_ts.add_gate('CNOT', controls=[6], targets = [7])
+nine_bit_ts.add_gate('CNOT', controls=[6], targets = [8])
+nine_bit_ts.add_gate('TOFFOLI', controls=[7, 8], targets=[6])
+nine_bit_ts.add_gate('SNOT', targets=[0])
+nine_bit_ts.add_gate('SNOT', targets=[3])
+nine_bit_ts.add_gate('SNOT', targets=[6])
+nine_bit_ts.add_gate('CNOT', controls=[0], targets = [3])
+nine_bit_ts.add_gate('CNOT', controls=[0], targets = [6])
+nine_bit_ts.add_gate('TOFFOLI', controls=[3, 6], targets=[0])
 
-#%% [markdown]
-# ## Example 
-# Example application using $(2|0> + i|1>)/\sqrt{5}$ and an error probability of 0.1
-#%%
-inpt = build_initial(1, 0.5j,  4)
-display("Input density matrix:")
-display(ket2dm(inpt).ptrace([0]))
-display("Density matrix for error with no correction:")
-display(apply_bitflip(ket2dm(inpt), error, 0).ptrace([0]))
-duped = gate_sequence_product(setup_circuit.propagators()) * inpt
-dupeddm = ket2dm(duped)
-err1 = apply_bitflip(dupeddm, error, 0)
-err2 = apply_bitflip(err1, error, 1)
-err3 = apply_bitflip(err2, error, 2)
-#%%
-measure_circuit_matrix = gate_sequence_product(measure_circuit.propagators())
-measured = measure_circuit_matrix * err3 * measure_circuit_matrix.dag()
-reduced = measured.ptrace([0, 1, 2])
-syndrome = measured.ptrace([3, 4])
+display(nine_bit_ts.png)
 
-#%%
-display("Density matrix for the 3 bits after all 3 errors:")
-display(reduced)
-no_flip_matrix = project_states(reduced, [0, 7])
-no_flip_corr = syndrome[0, 0] * no_flip_matrix
-#%%
-flip_3_matrix = project_states(reduced, [1, 6])
-flip_3_corr = syndrome[1, 1] * expand_oper(sigmax(), 3, 2) * flip_3_matrix * expand_oper(sigmax(), 3, 2).dag()
-#%%
-flip_2_matrix = project_states(reduced, [2, 5])
-flip_2_corr = syndrome[2, 2] * expand_oper(sigmax(), 3, 1) * flip_2_matrix * expand_oper(sigmax(), 3, 1).dag()
-#%%
-flip_1_matrix = project_states(reduced, [3, 4])
-flip_1_corr = syndrome[3, 3] * expand_oper(sigmax(), 3, 0) * flip_1_matrix * expand_oper(sigmax(), 3, 0).dag()
-#%%
-corrected = no_flip_corr + flip_1_corr + flip_2_corr + flip_3_corr
-display("Density matrix after corrections:")
-display(corrected.extract_states([0, 7]))
-
+# %%
+inp = build_initial(np.sqrt(0.2), np.sqrt(0.8) * 1j, 8).unit()
+display(inp.extract_states([0, 2**8], normalize=True))
+display(nine_bit_ts.user_gates['Error']())
+display( (nine_bit_ts.user_gates['Error']() *  inp.extract_states([0, 2**8], normalize=True) ).unit())
+display((gate_sequence_product(nine_bit_ts.propagators()) * inp).extract_states([0, 2**8], normalize=True))
 # %%
